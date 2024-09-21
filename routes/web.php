6b65f7a91457fd99b9c135ed6b69edf824367357
Vitor Souza <vitor.fabio.castro@gmail.com>
2024-09-21 13:26:40 -0300

    Fix route bug

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,16 @@
 <?php
 
 use App\Http\Controllers\MoradorController;
-use App\Http\Middleware\UsuarioTemPermissao;
-use App\Livewire\Morador\Show;
-use App\Livewire\Ongs\CreateOng;
 use App\Http\Controllers\OngController;
+use App\Http\Middleware\UsuarioTemPermissao;
 use App\Livewire\Comentario\ComentariosPendentes;
 use App\Livewire\Morador\CreateMorador;
-use App\Livewire\Morador\EditMorador;
 use App\Livewire\Morador\ListarTodos;
+use App\Livewire\Morador\Show;
+use App\Livewire\Ongs\CreateOng;
 use App\Livewire\Ongs\Doacao;
 use App\Livewire\SobreNos;
 use App\Livewire\Somenos;
-use App\Models\Morador;
-use App\Models\Ong;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -27,7 +24,7 @@ Route::get('dashboard', [MoradorController::class, 'index'])
 Route::get('/sobre-nos', SobreNos::class)->name('sobre_nos');
 Route::get('/some-nos', Somenos::class)->name('some_nos');
 Route::get('/fale-conosco', function () {
-    return "FALE CONOSCO"
+    return 'FALE CONOSCO';
 })->name('fale_conosco');
 
 Route::view('profile', 'profile')
@@ -36,14 +33,14 @@ Route::view('profile', 'profile')
 
 Route::prefix('/ongs')->group(function () {
     Route::get('/create', CreateOng::class)
-        ->middleware(['auth', UsuarioTemPermissao::class . ':comum'])
+        ->middleware(['auth', UsuarioTemPermissao::class.':comum'])
         ->name('ongs.create');
 
     Route::post('/store', [OngController::class, 'store'])
-        ->middleware(['auth', UsuarioTemPermissao::class . ':comum'])
+        ->middleware(['auth', UsuarioTemPermissao::class.':comum'])
         ->name('ongs.store');
     Route::get('/dashboard', [OngController::class, 'index'])
-        ->middleware(UsuarioTemPermissao::class . ':admin')
+        ->middleware(UsuarioTemPermissao::class.':admin')
         ->name('ongs.dashboard');
 
     Route::get('/doacao', Doacao::class)->name('ongs.doacao');
@@ -54,22 +51,22 @@ Route::prefix('/ongs')->group(function () {
 Route::prefix('/moradores')->middleware('auth')->group(function () {
 
     Route::get('/create', CreateMorador::class)
-        ->middleware(UsuarioTemPermissao::class . ':admin')
+        ->middleware(UsuarioTemPermissao::class.':admin')
         ->name('morador.create');
 
     Route::get('/show/{id}', Show::class)
         ->name('morador.show');
 
     Route::get('/all', ListarTodos::class)
-        ->middleware(UsuarioTemPermissao::class . ':admin')
+        ->middleware(UsuarioTemPermissao::class.':admin')
         ->name('morador.all');
 
     Route::get('/edit/{id}', CreateMorador::class)
-        ->middleware(UsuarioTemPermissao::class . ':admin')
+        ->middleware(UsuarioTemPermissao::class.':admin')
         ->name('morador.edit');
 
     Route::get('/destroy/{id}', [MoradorController::class, 'destroy'])
-        ->middleware(UsuarioTemPermissao::class . ':admin')
+        ->middleware(UsuarioTemPermissao::class.':admin')
         ->name('morador.destroy');
 
     Route::get('/find', [MoradorController::class, 'find'])
@@ -82,4 +79,4 @@ Route::prefix('/comentarios')->group(function () {
     Route::get('/excluir/{id_comentario}', [ComentariosPendentes::class, 'excluir'])->name('comentarios.pendentes.excluir');
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
